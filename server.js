feb97191dc12d968b63e478c540edbac3b3b877e
AleCigolini <cigolini.ale@gmail.com>
2024-09-19 21:29:49 -0300

    Created some routes

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ import http from "http";
 
 const PORT = 3000;
 
+const routes = {
+    "/": "Node.js course",
+    "/livros": "Livros route",
+    "/autores": "Autores route"
+}
+
 const server = http.createServer((req, res) =>  {
     // HEADERS
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Node.js course");
+    res.end(routes[req.url]);
 });
 
 server.listen(PORT, () => {
